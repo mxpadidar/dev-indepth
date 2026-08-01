0255ef6ce6de915abb60b490859fb251e517a34b
mxpadidar <mxpadidar@gmail.com>
2026-08-01 23:33:32 +0330

    feat: add suse-mono font using font provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mxpadidar.github.io",
   integrations: [sitemap()],
+
+  fonts: [
+    {
+      name: "SUSE Mono",
+      cssVariable: "--font-suse-mono",
+      provider: fontProviders.fontsource(),
+      styles: ["normal"],
+      weights: ["300 700"],
+    },
+  ],
 });
