12d1fba6f5c70cd260a3d2f5f2c25e12f881389e
mxpadidar <mxpadidar@gmail.com>
2026-08-09 23:38:36 +0330

    feat: content collector config

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import { z } from "astro/zod";
+
+const blogPosts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()).default([]),
+      author: z.string().default("mxpadidar"),
+      draft: z.boolean().default(false),
+      heroImagePath: z.string(),
+      publishedAt: z.coerce.date(),
+    }),
+});
+
+export const collections = { blogPosts: blogPosts };
