1bfe3bc251df3bb44f9ac71ad6d6ef91e45d5bd8
mxpadidar <mxpadidar@gmail.com>
2026-08-12 01:09:43 +0330

    feat: add syntax highlighting with Expressive Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,21 @@ import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://mxpadidar.github.io",
-  integrations: [sitemap(), icon()],
+  integrations: [
+    sitemap(),
+    icon(),
+    expressiveCode({
+      themes: ["tokyo-night"],
+      defaultProps: {
+        showLineNumbers: true,
+      },
+    }),
+  ],
 
   fonts: [
     {
