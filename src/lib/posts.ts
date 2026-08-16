0e20b9328c08a8663b4bcf57756b2fcbedfdb98f
mxpadidar <mxpadidar@gmail.com>
2026-08-16 20:08:23 +0330

    feat: add paginations to posts page

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,5 +1,7 @@
 import { getCollection } from "astro:content";
 
+export const POSTS_PER_PAGE = 6;
+
 export async function getPosts(limit?: number) {
   const posts = await getCollection("blogPosts", ({ data }) => !data.draft);
   const sortedPosts = posts.toSorted(
@@ -7,4 +9,15 @@ export async function getPosts(limit?: number) {
   );
 
   return limit === undefined ? sortedPosts : sortedPosts.slice(0, limit);
+}
+
+export async function getPostPages() {
+  const posts = await getPosts();
+  const pages = [];
+
+  for (let i = 0; i < posts.length; i += POSTS_PER_PAGE) {
+    pages.push(posts.slice(i, i + POSTS_PER_PAGE));
+  }
+
+  return pages;
 }
